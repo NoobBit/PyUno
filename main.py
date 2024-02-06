875b90df6bac59eae7044868b0f02b22ec804052
P Deol <pdeol@Ds-MacBook-Air.local>
2024-02-05 22:55:03 -0800

    Test Commit

---
+++ main.py
@@ -0,0 +1,128 @@
+import random
+
+class Card:
+    def __init__(self, type, color):
+        self.type = type
+        self.color = color
+
+class Game:
+    def __init__(self):
+        self.total_cards, self.normal_cards = self.create_all_cards()
+        self.cards_of_all_players = self.setup_player_cards(2, 7)
+        self.first_card = self.choose_first_card()
+
+    def setup(self):
+        self.create_players()
+
+    def create_all_cards(self):
+        #TODO: Unhardcode later
+        repeat_once_type = ["0"]
+        repeat_twice_types = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "reverse", "skip", "+2"]
+        repeat_four_types = ["wild", "+4"]
+        colors = ["red", "yellow", "green", "blue"]
+
+        red_cards = []
+        yellow_cards = []
+        green_cards = []
+        blue_cards = []
+        misc_cards = []
+
+        normal_cards = []
+        total_cards = []
+
+        for color in colors:
+            if color == "red":
+                for ro_card in repeat_once_type:
+                    red_cards.append(Card(ro_card, "red"))
+                for rt_card in repeat_twice_types:
+                    red_cards.append(Card(rt_card, "red"))
+                    red_cards.append(Card(rt_card, "red"))
+
+            if color == "yellow":
+                for ro_card in repeat_once_type:
+                    yellow_cards.append(Card(ro_card, "yellow"))
+                for rt_card in repeat_twice_types:
+                    yellow_cards.append(Card(rt_card, "yellow"))
+                    yellow_cards.append(Card(rt_card, "yellow"))
+
+            if color == "green":
+                for ro_card in repeat_once_type:
+                    green_cards.append(Card(ro_card, "green"))
+                for rt_card in repeat_twice_types:
+                    green_cards.append(Card(rt_card, "green"))
+                    green_cards.append(Card(rt_card, "green"))
+
+            if color == "blue":
+                for ro_card in repeat_once_type:
+                    blue_cards.append(Card(ro_card, "blue"))
+                for rt_card in repeat_twice_types:
+                    blue_cards.append(Card(rt_card, "blue"))
+                    blue_cards.append(Card(rt_card, "blue"))
+
+        for rf_card in repeat_four_types:
+            misc_cards.append(Card(rf_card, "none"))
+            misc_cards.append(Card(rf_card, "none"))
+            misc_cards.append(Card(rf_card, "none"))
+            misc_cards.append(Card(rf_card, "none"))
+
+        #Add all Cards
+        normal_cards.extend(red_cards)
+        normal_cards.extend(yellow_cards)
+        normal_cards.extend(green_cards)
+        normal_cards.extend(blue_cards)
+        total_cards.extend(normal_cards)
+        total_cards.extend(misc_cards)
+
+        return [total_cards, normal_cards]
+
+   
+
+    def setup_players_cards(self, player_amount, cards_per_player):
+        cards_of_all_players = []
+        
+        for player in range(player_amount):
+            player_cards = []
+            for card in range(cards_per_player):
+                player_cards.append(self.total_cards[random.randint(0, len(self.total_cards) - 1)])
+            cards_of_all_players.append(player_cards)
+
+        return cards_of_all_players
+
+    def choose_first_card(self):
+        #TODO: Remove +2, reverse, and skip from normal_cards
+        first_card = self.normal_cards[random.randint(0, len(self.normal_cards) - 1)]
+
+        return first_card
+
+class Player:
+    def __init__(self, cards):
+        self.cards = cards
+
+game = Game()
+
+
+
+print(game.first_card.type)
+
+# for i in game.cards_of_all_players:
+    # for j in i:
+    #     print(j.type)
+    # print("\n\n")
+#Give each player their cards
+player1_cards = []
+player2_cards = []
+
+# for card in range(7):
+#     player1_cards.append(total_cards[random.randint(0, len(total_cards) - 1)])
+
+# for card in range(7):
+#     player2_cards.append(total_cards[random.randint(0, len(total_cards) - 1)])
+
+# #Create and filter First Card
+# first_card = normal_cards[random.randint(0, len(normal_cards) - 1)]
+
+# for p1_card in player1_cards:
+#     print(p1_card.color + ": " + p1_card.type)
+# print("\n\n")
+# for p2_card in player1_cards:
+#     print(p2_card.color + ": " + p2_card.type)
---
+++ win.py
@@ -0,0 +1,7 @@
+import tkinter
+
+root = tkinter.Tk()
+
+root.geometry("500x500")
+
+root.mainloop()
