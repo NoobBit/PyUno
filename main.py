0a148e24bcfbe3207b565d67eeadb0451c7e7f84
P Deol <pdeol@Ds-MacBook-Air.local>
2024-06-03 21:59:04 -0700

    Fixed Player Choice Of Card Validation Using Loops AND Implemented Drawing Functionality

--- main.py
+++ main.py
@@ -22,12 +22,11 @@ class Game:
         self.last_card_placed = self.first_card
         print(f"The beginning card is a {self.last_card_placed.color} {self.last_card_placed.type}!")
 
-        print(f"\nIt is now player {self.current_player_turn.id}'s turn! Other player(s), please look away from the screen as player {self.current_player_turn.id} chooses their card.")
+        print(f"\nIt is now player {self.current_player_turn.id + 1}'s turn! Other player(s), please look away from the screen as player {self.current_player_turn.id + 1} chooses their card.")
         input("Press enter to view cards... ")
         os.system('cls' if os.name=='nt' else 'clear')
 
         self.gameloop_player_choice()
-
 
     def create_all_cards(self):
         #TODO: Unhardcode later
@@ -103,13 +102,16 @@ class Game:
         player_cards = []
 
         for card in range(cards_per_player):
-            player_cards.append(self.total_cards[random.randint(0, len(self.total_cards) - 1)])
+            card_chosen = random.choice(self.total_cards)
+            player_cards.append(card_chosen)
+            self.total_cards.remove(card_chosen)
 
         return player_cards
     
     def gameloop_player_choice(self):
         player_choice = None
-        while player_choice == None or not player_choice.isdigit() or int(player_choice) > len(self.current_player_turn.cards) or int(player_choice) < 1:
+        is_not_acceptable_card = True
+        while is_not_acceptable_card:
             print(f"The last card placed is a {self.last_card_placed.color} {self.last_card_placed.type}\n")
             print("You have the following cards...")
 
@@ -121,20 +123,88 @@ class Game:
                 else:
                     print(f"[{card_increment}] {card.color} {card.type}")
         
-            #TODO: Implement Option To Draw Cards If No Cards Match
-            player_choice = input("\nPlease choose the number of the card (found on the left) for the card you would like to place: ")
+            
+            print("\nPlease choose the number of the card (found on the left) for the card you would like to place. Or, if you cannot place a card because it doesn't match the last card placed down, please type in \"DRAW\" to draw a new card.")
+            player_choice = input()
+            if player_choice.lower() == "draw":
+                self.gameloop_draw_card(self.current_player_turn)
+                break
+            elif player_choice.lower() == "exit" or player_choice.lower() == "quit":
+                exit(0)
 
-            if player_choice == None or not player_choice.isdigit() or int(player_choice) > len(self.current_player_turn.cards) or int(player_choice) < 1:
+            if not player_choice.isdigit() or int(player_choice) > len(self.current_player_turn.cards) or int(player_choice) < 1:
                 print("\nUh oh. It doesn't seem like this card is in your list. Please enter the card id number (found on the left) to place that card.")
                 input("Please press Enter to continue... ")
+                is_not_acceptable_card = True
                 os.system('cls' if os.name=='nt' else 'clear')
+            elif self.current_player_turn.cards[int(player_choice) - 1].color != "none":
+                if self.current_player_turn.cards[int(player_choice) - 1].type != self.last_card_placed.type and self.current_player_turn.cards[int(player_choice) - 1].color != self.last_card_placed.color:
+                    print("\nUh oh. It doesn't seem like this card can be placed. Please choose a card that matches in type, color, or number with the last card placed. If you do not have any cards that meet this requirement, please type in \"DRAW\" to draw a new card.")
+                    input("Please press Enter to continue... ")
+                    is_not_acceptable_card = True
+                    os.system('cls' if os.name=='nt' else 'clear')
+                else:
+                    is_not_acceptable_card = False
+                    break
             else:
+                is_not_acceptable_card = False
                 break
-        card_chosen = self.current_player_turn.cards[int(player_choice) - 1]
-        if card_chosen.color != "none":
-            print(f"You have chosen the card {card_chosen.color} {card_chosen.type}")
-        else:
-            print(f"You have chosen the card {card_chosen.type}")
+        if is_not_acceptable_card == False:
+            card_chosen = self.current_player_turn.cards[int(player_choice) - 1]
+            self.gameloop_play_card(card_chosen)
+            if card_chosen.color != "none":
+                print(f"\nYou have chosen the card {card_chosen.color} {card_chosen.type}")
+            else:
+                print(f"\nYou have chosen the card {card_chosen.type}")
+    
+    def gameloop_draw_card(self, player_to_draw_to):
+        drawn_card = random.choice(self.total_cards)
+        player_to_draw_to.cards.append(drawn_card)
+        self.total_cards.remove(drawn_card)
+
+        is_not_acceptable_option = True
+        while is_not_acceptable_option:
+            if drawn_card.color == "none":
+                print(f"\nYou have drawn a {drawn_card.type} card")
+            else:
+                print(f"\nYou have drawn a {drawn_card.color} {drawn_card.type} card")
+        
+            print("\nWould you like to...\n[1] Play the card\n[2] Save it and end turn\n[3] Save it and go back to all cards.")
+            player_choice = input()
+
+            if not player_choice.isdigit() or (int(player_choice) != 1 and int(player_choice) != 2 and int(player_choice) != 3):
+                print("\nUh oh. Invalid Option. Please choose 1, to play the card, 2, to save it and end your turn, or 3, to save it and return to all your cards.")
+                input("Please press Enter to continue... ")
+                is_not_acceptable_option = True
+                os.system('cls' if os.name=='nt' else 'clear')
+            elif int(player_choice) == 1:
+                if drawn_card.color != "none":
+                    if drawn_card.type != self.last_card_placed.type and drawn_card.color != self.last_card_placed.color:
+                        print("\nUh oh. It doesn't seem like this card can be placed. Please choose either 2, to save it and end your turn, or 3, to save it and return to all your cards.")
+                        input("Please press Enter to continue... ")
+                        is_not_acceptable_option = True
+                        os.system('cls' if os.name=='nt' else 'clear')
+                    else:
+                        self.gameloop_play_card(player_to_draw_to, drawn_card)
+                        break
+                else:
+                    self.gameloop_play_card(player_to_draw_to, drawn_card)
+                    break
+                break
+            elif int(player_choice) == 2:
+                is_not_acceptable_option = False
+                os.system('cls' if os.name=='nt' else 'clear')
+                print(f"Player {player_to_draw_to.id + 1} drew a card and chose to keep it")
+                break
+            elif int(player_choice) == 3:
+                is_not_acceptable_option = False
+                os.system('cls' if os.name=='nt' else 'clear')
+                self.gameloop_player_choice()
+                break
+    
+    def gameloop_play_card(self, player, card):
+        #TODO: Implement
+        pass
            
     def choose_first_card(self):
         #TODO: Remove +2, reverse, and skip from normal_cards
@@ -156,9 +226,3 @@ class Player:
 
 game = Game(2, 7)
 game.gameloop()
-
-# for player in game.players:
-#     print(player.id)
-#     for card in player.cards:
-#         print(card.color + ": " + card.type)
-#     print("\n")
